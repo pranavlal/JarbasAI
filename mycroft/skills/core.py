sascristian/HeyberryAI

External enable/disable intent
Allow intents to be enabled or disabled from outside

needed for example in my intent_layers helper class to control what intents are active

mycroft/skills/core.py
@@ -220,6 +220,8 @@ class MycroftSkill(object):
             self.emitter = emitter
             self.enclosure = EnclosureAPI(emitter)
             self.__register_stop()
+            self.emitter.on('enable_intent', self.handle_enable_intent)
+            self.emitter.on('disable_intent', self.handle_disable_intent)
 
     def __register_stop(self):
         self.stop_time = time.time()
@@ -261,12 +263,23 @@ class MycroftSkill(object):
         if handler:
             self.emitter.on(intent_parser.name, receive_handler)
             self.events.append((intent_parser.name, receive_handler))
+    
+    def handle_enable_intent(self, message):
+        intent_name = message.data["intent_name"]
+        self.enable_intent(intent_name)
 
+    def handle_disable_intent(self, message):
+        intent_name = message.data["intent_name"]
+        self.disable_intent(intent_name)
+        
     def disable_intent(self, intent_name):
-        """Disable a registered intent"""
-        logger.debug('Disabling intent ' + intent_name)
-        name = self.name + ':' + intent_name
-        self.emitter.emit(Message("detach_intent", {"intent_name": name}))
+         """Disable a registered intent"""
+        for (name, intent) in self.registered_intents:
+            if name == intent_name:
+                logger.debug('Disabling intent ' + intent_name)
+                name = self.name + ':' + intent_name
+                self.emitter.emit(Message("detach_intent", {"intent_name": name}))
+                break
 
     def enable_intent(self, intent_name):
         """Reenable a registered intent"""
@@ -277,9 +290,6 @@ class MycroftSkill(object):
                 self.register_intent(intent, None)
                 logger.debug('Enabling intent ' + intent_name)
                 break
-            else:
-                logger.error('Could not enable ' + intent_name +
-                             ', it hasn\'t been registered.')
 
     def register_vocabulary(self, entity, entity_type):
         self.emitter.emit(Message('register_vocab', {

pranavlal/JarbasAI

mycroft/skills/core.py
@@ -301,6 +301,8 @@ class MycroftSkill(object):
             self.emitter = emitter
             self.enclosure = EnclosureAPI(emitter, self.name)
             self.__register_stop()
+            self.emitter.on('enable_intent', self.handle_enable_intent)
+            self.emitter.on('disable_intent', self.handle_disable_intent)
 
     def __register_stop(self):
         self.stop_time = time.time()
@@ -769,11 +771,22 @@ class MycroftSkill(object):
             "name": name
         }))
 
+    def handle_enable_intent(self, message):
+        intent_name = message.data["intent_name"]
+        self.enable_intent(intent_name)
+
+    def handle_disable_intent(self, message):
+        intent_name = message.data["intent_name"]
+        self.disable_intent(intent_name)
+
     def disable_intent(self, intent_name):
         """Disable a registered intent"""
-        LOG.debug('Disabling intent ' + intent_name)
-        name = str(self.skill_id) + ':' + intent_name
-        self.emitter.emit(Message("detach_intent", {"intent_name": name}))
+        for (name, intent) in self.registered_intents:
+            if name == intent_name:
+                LOG.debug('Disabling intent ' + intent_name)
+                name = str(self.skill_id) + ':' + intent_name
+                self.emitter.emit(Message("detach_intent", {"intent_name": name}))
+                break
 
     def enable_intent(self, intent_name):
         """Reenable a registered intent"""